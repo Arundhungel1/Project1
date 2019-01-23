128247534e63d967e15432941318d7721fdc5e73
Arun Dhungel <arundhungel.npa@gmail.com>
2019-01-23 15:55:40 +1100

    user_id for works migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_23_002551) do
+ActiveRecord::Schema.define(version: 2019_01_23_045501) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,7 +45,6 @@ ActiveRecord::Schema.define(version: 2019_01_23_002551) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
-    t.index ["user_id"], name: "index_workouts_on_user_id"
   end
 
   add_foreign_key "exercises", "workouts"
