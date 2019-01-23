95dfd480e639eea63b3f87b685d06fa99ebfe867
Arun Dhungel <arundhungel.npa@gmail.com>
2019-01-23 14:08:23 +1100

    fixing create.

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -12,7 +12,7 @@ class WorkoutsController < ApplicationController
   end
 
   def create
-    @workout = Workout.new(workout_params)
+    @workout = Workout.create(workout_params)
     if @workout.save
       current_user.workouts << @workout
       redirect_to @workout
